4b558d049654d7a18e1cf88af822874db62b7280
Marfim <136606147+gmarfim@users.noreply.github.com>
2024-05-02 13:53:07 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,59 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+app.get('/', function(req, res) {
+  res.send('Hello World!');
+});
+
+var port = 3000;
+
+// iniciando o processo do servidor
+app.listen(port, function() {
+  console.log(`App de Exemplo escutando na porta http://localhost:${port}/`);
+});
+
+
+// SOMA
+// a, b = numeros a somar
+
+app.post('/soma', function (req, res) {
+    var body = req.body;
+    var resultado = soma(body.a, body.b);
+    
+    res.send(`O resultado da soma de ${body.a} e ${body.b} é ${resultado}`);
+  });
+
+function soma(a, b) {
+    return a + b;
+}
+
+// FIBO
+// a = quantidade de números
+
+app.post('/fibo', function (req, res) {
+    var body = req.body;
+    var resultado = fibonacci(body.a);
+    
+    res.send(`Fibonacci representado por ${body.a} números é ${resultado}`);
+});
+
+
+function fibonacci(a) {
+    let primeiro = 0;
+    let segundo = 1;
+    let sequenciaDeNumeros = '0 1';
+
+    for (let x = 0; x < a - 2; x++) {
+        let soma = primeiro + segundo;
+        sequenciaDeNumeros += " " + soma;
+        let aux = soma;
+        primeiro = segundo;
+        segundo = soma;
+    }
+
+    return sequenciaDeNumeros;
+}
+
+
